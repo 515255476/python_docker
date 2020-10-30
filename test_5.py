ef888451afc84b7da6fb0ab142d3f8fb5145f138
yangyang <18004261646@163.com>
2020-10-30 15:55:28 +0800

    传递参数

--- test_5.py
+++ test_5.py
@@ -1,13 +1,13 @@
 import sys
 p1=sys.argv[1]
 
-
+# p1='test'
 
 if p1=='test':
     url='测试url'
     oracle='测试数据库'
 elif p1=='dev':
     url='开发url'
-    oralce='开发测试库'
+    oracle='开发测试库'
 
-print(url + '' + oralce)
+print(url + '' + oracle)
