dcfc5221bdbe03eb2bf69ddd0d0d7c044150bd2e
yangyang <18004261646@163.com>
2020-10-30 14:21:17 +0800

    PWD

--- test5.py
+++ test5.py
@@ -1,4 +1,15 @@
 import os
-name=os.environ['name']
-password=os.environ['password']
-print(name + password)
+
+
+env=os.getenv('PWD')
+# env=os.environ['env']
+print(env)
+# if env=='test':
+#     api_root_url="测试：http://10.43/xtrading"
+#     oracle="oracle测试地址"
+# else:
+#     api_root_url = "开发：http://10.43/xtrading"
+#     oracle = "oracle开发地址"
+#
+# print(api_root_url)
+# print(oracle)
